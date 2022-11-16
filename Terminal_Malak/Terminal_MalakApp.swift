66e512127ce8a5d5e75000bf56a95d166ab6da3e
MalakHulimi <malakhulimi@gmail.com>
2022-11-16 15:32:39 +0300

    Initial Commit

---
+++ Terminal_Malak/Terminal_MalakApp.swift
@@ -0,0 +1,17 @@
+//
+//  Terminal_MalakApp.swift
+//  Terminal_Malak
+//
+//  Created by Malak  on 22/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Terminal_MalakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
